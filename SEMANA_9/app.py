57b47fd23d4488cc30e29a32f91ca32a332baff8
Saidreyesp <sr.reyesp@uea.edu.ec>
2026-02-16 11:50:38 -0500

    Add files via upload

--- SEMANA_9/app.py
+++ SEMANA_9/app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Ruta principal
-@app.route("/")
-def inicio():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Ruta dinámica (ejemplo pedido en el deber)
-@app.route("/plato/<nombre>")
-def plato(nombre):
-    return f"Plato seleccionado: {nombre} - Restaurante El Gran Sabor"
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Ruta para la página de productos
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
