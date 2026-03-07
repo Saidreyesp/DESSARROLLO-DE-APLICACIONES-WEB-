1a0ba806609a6b25f2c82a354a63a069a0d670a9
Saidreyesp <sr.reyesp@uea.edu.ec>
2026-03-07 11:11:46 -0500

    feat: add GitHub Pages demo with static HTML files

--- SEMANA_9/SEMANA_9/app.py
+++ SEMANA_9/SEMANA_9/app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -17,3 +18,24 @@ def productos():
 
 if __name__ == "__main__":
     app.run(debug=True)
+=======
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Ruta para la página de productos
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+>>>>>>> de0d8b5 (Resueltos los conflictos de fusión)
